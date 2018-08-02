4eed15e3cda5c2b0b215607c4acc8d6e4188b929
Fever sun <Feverun@FeverdeMacBook-Pro.local>
2018-08-02 17:37:11 +0800

    Init Repo

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import logo from '../images/logo-designcode.svg'
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+    <Link to="/"><img src={logo} width="30" /></Link>
+    <Link to="/courses">courses</Link>
+    <Link to="/downloads">downloads</Link>
+    <Link to="/workshops">workshops</Link>
+    <Link to="/buy"><button>buy</button></Link>
+    </div>
+  </div>
+)
+
+export default Header
